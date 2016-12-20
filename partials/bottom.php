be39b617f1df77fdb29552492abe4fb9b69d04c1
Cara McIlnay <cara.mcilnay@aofl.com>
2016-12-20 14:29:27 -0800

    gh-pages: breakinging out the top and bottom.php

---
+++ partials/bottom.php
@@ -0,0 +1,23 @@
+        <!-- ******FOOTER****** -->
+        <footer class="footer">
+            <div class="container text-center">
+                <small class="copyright">Designed with <i class="fa fa-heart"></i> by <a href="http://themes.3rdwavemedia.com"
+                                                                                         target="_blank">Xiaoying Riley</a> for
+                    developers
+                </small>
+            </div><!--//container-->
+        </footer><!--//footer-->
+
+        <!-- Javascript -->
+        <script type="text/javascript" src="assets/plugins/jquery-1.11.2.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/jquery-rss/dist/jquery.rss.min.js"></script>
+        <!-- github activity plugin -->
+        <script type="text/javascript" src="assets/plugins/github-activity/dist/mustache/mustache.min.js"></script>
+        <script type="text/javascript" src="assets/plugins/github-activity/dist/github-activity-0.1.1.min.js"></script>
+        <!-- custom js -->
+        <script type="text/javascript" src="assets/js/main.js"></script>
+    </body>
+</html>
+
