3a7bd95c17d8905a35769ef0e9880e0ef24181de
Cara McIlnay <cara.mcilnay@aofl.com>
2016-12-21 18:21:11 -0800

    gh-pages: updated blog to be formatted

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,28 +28,25 @@ jQuery(document).ready(function ($) {
       'https://caramacc.blogspot.com/feeds/posts/default?alt=rss',
 
       {
-      //  // how many entries do you want?
-      //  // default: 4
-      //  // valid values: any integer
-      //  limit: 3,
-      //
+        // how many entries do you want?
+        // default: 4
+        // valid values: any integer
         ssl: true,
-      //
-      //  // the effect, which is used to let the entries appear
-      //  // default: 'show'
-      //  // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-      //  effect: 'show',
-      //
-      //  // outer template for the html transformation
-      //  // default: "<ul>{entries}</ul>"
-      //  // valid values: any string
-      //  layoutTemplate: "<div class='item'>{entries}</div>",
-      //
-      //  // inner template for each entry
-      //  // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-      //  // valid values: any string
-      //  entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-      //
+
+        // the effect, which is used to let the entries appear
+        // default: 'show'
+        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+        effect: 'slideFastSynced',
+
+        // outer template for the html transformation
+        // default: "<ul>{entries}</ul>"
+        // valid values: any string
+        layoutTemplate: "<div class='item'>{entries}</div>",
+
+        // inner template for each entry
+        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+        // valid values: any string
+        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
       }
   );
 
